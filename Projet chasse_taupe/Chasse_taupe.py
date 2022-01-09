fc5e261f017b54b2d200f703eecc8db1427f6194
Cabochards <90553363+Cabochards@users.noreply.github.com>
2022-01-09 17:36:10 +0100

    projet chasse taupe v2

--- Projet chasse_taupe/Chasse_taupe.py
+++ Projet chasse_taupe/Chasse_taupe.py
@@ -1,24 +1,31 @@
 import pygame
+from pygame.sprite import collide_rect
 import Classe
 from pygame.display import set_icon
 
 
 pygame.init()
 
+vitesse=pygame.time.Clock()
 #creation de la taille de la fenetre 
 
-screen=pygame.display.set_mode((864,816))
+screen=pygame.display.set_mode((800,600))
 pygame.display.set_caption('Chasse-taupe')
 
 
 Icone = pygame.image.load('images/Mole.png')
 pygame.display.set_icon(Icone)
 
-background= pygame.image.load('images/la foret.png')
+background= pygame.image.load('images/bg.jpg')
 
-game=Classe.Game()
-player=Classe.Player()
-taupe=Classe.Taupe()
+game= Classe.Game()
+player= Classe.Player()
+Ennemis= []
+
+for i in range(Classe.Taupe.nombreEnnemis):
+    taupe= Classe.Taupe()
+    Ennemis.append(taupe)
+
 pygame.mouse.set_visible(False)
 #On maintient la fenetre ouverte
 running= True
@@ -26,21 +33,49 @@ running= True
 while running :
     
     screen.blit(background,(0,0))
-
-
     player.rect.center = pygame.mouse.get_pos() 
-    screen.blit(player.image,player.rect) # applique le curseur
+    player.Barrede_vie(screen)
+    player.vie-= 0.1 #vitesse de la barre de vie
     
-    screen.blit(taupe.image,taupe.rect)
-    
-    pygame.display.flip()
-
-   
     for event in pygame.event.get():
+        
         if event.type== pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False # running est sur False
+        
+        
+        
+        for a in Ennemis:
+            Enemmies_envie=[]
+            if player.rect.colliderect(a.rect) and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                a.vie-=player.degats       
+                player.Gainde_vie()
+                a.Spawn()
+                if a.Spawn():
+                   Enemmies_envie.append(a)
+            
+            if not player.rect.colliderect(a.rect) and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:        
+                a.Sustain()           
+                player.perte_devie()
+                a.Spawn()
+            
+            if player.vie_max < player.vie:
+                player.vie= player.vie_max
+            
+            
+                
+                      
+            
         if event.type==pygame.QUIT:
-            running = False
+           running = False
+    screen.blit(player.image,player.rect) # applique le curseur
+    
+    for k in Ennemis:
+        screen.blit(k.image,k.rect)
+        
+        
+    
+    pygame.display.flip()
+    vitesse.tick(169)
 
 pygame.quit() 
--- Projet chasse_taupe/Classe.py
+++ Projet chasse_taupe/Classe.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import time
 
+
 class Game:
     
     def __init__(self):
@@ -12,24 +13,54 @@ class Game:
 class Player(pygame.sprite.Sprite):
     
     def __init__(self) :
-        super().__init__()
-        self.vie= 5
-        self.vie_max=5
-        self.degats=1
-        self.image= pygame.image.load('images/hammer_210x210.png')
-        self.rect= self.image.get_rect()
-    
-    #def HP():
-    #def Frapper():
-   
         
+        super().__init__()
+        
+        self.vie=  500
+        self.vie_max= 500
+        self.degats= 50
+        self.bdv=500
+        self.image= pygame.image.load('images/crosshair_1_50.png')
+        self.rect= self.image.get_rect()
+   
+    def Barrede_vie(self,screen):
+       
+       pygame.draw.rect(screen,(244,203,164),[10,20,self.vie_max,10])
+       pygame.draw.rect(screen,(244,121,131),[10,20,self.vie,10])
+   
+    def Gainde_vie(self):
+        self.vie += 30
+
+    def perte_devie(self):
+        self.vie -=10
+
 class Taupe():
+    
     def __init__(self):
-        self.vie=0.1
-        self.vie=0.1
-        self.degats=1
-        self.image=pygame.image.load('images/mole180x180.png')
-        self.rect=self.image.get_rect()
-    #def Spawn():
-    #def Morsure(): 
-    #def TypeTaupe():
+        
+        taupeimage=['images/alien.png','images/mcdo.png','images/basique.png']
+        self.image=pygame.image.load(random.choice(taupeimage))
+    
+        self.vie= 50
+        self.vie_max= 50
+        self.degats= 1
+        self.rect= self.image.get_rect()
+        self.rect.x= 480
+        self.rect.y= 345
+    
+    nombreEnnemis= 2
+    
+    def Spawn(self):
+        
+        positions = [  (480,345), (140,450),(20,362),(280,370),(710,410) ]
+        
+        nid= random.choice(positions)
+        self.rect.x= nid[0]  
+        self.rect.y= nid[1]
+        self.vie+=1
+        
+    
+    def Sustain(self):
+        self.vie+=1
+        if self.vie > self.vie_max:
+            self.vie= self.vie_max
