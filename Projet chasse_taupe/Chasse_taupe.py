59b696e1b792b8e0b939e900007b83364249cc11
Cabochards <90553363+Cabochards@users.noreply.github.com>
2022-01-09 23:33:26 +0100

    Projet chasse taupe final stp ouvre celui la

--- Projet chasse_taupe/Chasse_taupe.py
+++ Projet chasse_taupe/Chasse_taupe.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.draw import rect
 from pygame.sprite import collide_rect
 import Classe
 from pygame.display import set_icon
@@ -6,7 +7,7 @@ from pygame.display import set_icon
 
 pygame.init()
 
-vitesse=pygame.time.Clock()
+vitesse=pygame.time.Clock()#pour les fps
 #creation de la taille de la fenetre 
 
 screen=pygame.display.set_mode((800,600))
@@ -17,14 +18,14 @@ Icone = pygame.image.load('images/Mole.png')
 pygame.display.set_icon(Icone)
 
 background= pygame.image.load('images/bg.jpg')
+background_rect= background.get_rect()
 
-game= Classe.Game()
 player= Classe.Player()
 Ennemis= []
 
-for i in range(Classe.Taupe.nombreEnnemis):
+for i in range(Classe.Taupe.nombreEnnemis): #sert pr le nombre de taupe qui apparait sur le terrain tant qu il y aura pas le nombre d ennemis max il en  refera automatiquement respawn
     taupe= Classe.Taupe()
-    Ennemis.append(taupe)
+    Ennemis.append(taupe)#On cree une liste taupe qui nous permettre de faire apparaitre les ennemis
 
 pygame.mouse.set_visible(False)
 #On maintient la fenetre ouverte
@@ -35,39 +36,56 @@ while running :
     screen.blit(background,(0,0))
     player.rect.center = pygame.mouse.get_pos() 
     player.Barrede_vie(screen)
-    player.vie-= 0.1 #vitesse de la barre de vie
+    player.vie-= 0.1 # vitesse de la barre de vie
     
     for event in pygame.event.get():
         
         if event.type== pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False # running est sur False
-        
+        if event.type==pygame.QUIT:
+           running = False
         
         
         for a in Ennemis:
-            Enemmies_envie=[]
+
+            if a.vie == 0:
+                a.Spawn()
+              
             if player.rect.colliderect(a.rect) and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                a.vie-=player.degats       
+               
+                a.vie-=player.degats
                 player.Gainde_vie()
-                a.Spawn()
-                if a.Spawn():
-                   Enemmies_envie.append(a)
-            
-            if not player.rect.colliderect(a.rect) and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:        
-                a.Sustain()           
-                player.perte_devie()
-                a.Spawn()
-            
-            if player.vie_max < player.vie:
-                player.vie= player.vie_max
-            
+                
+            elif player.rect.colliderect(background_rect) and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1  :        
+            #On met une condition si on touche l ecran avec notre souris on perd de la vie on en fait gagner aux autres
             
                 
-                      
+                if not player.rect.colliderect(a.rect) :
+                # on redistribue les pv car on a pas touche la hitbox de la taupe
+    
+                    a.Sustain()
+                   
+                    if a.vie == 50 :
+                        a.image= pygame.image.load("images/basique.png")
+                        a.vie-=50
+                    
+                    if a.vie == 100:
+                        a.image= pygame.image.load("images/alien.png") 
+                        
+                
+                    if a.vie == 150:
+                        a.image= pygame.image.load("images/mcdo.png")
+                   
+                player.perte_devie()
+               
             
-        if event.type==pygame.QUIT:
-           running = False
+            if player.vie_max < player.vie:#on fait en sorte que nos pv ne depasse jamais 500
+                player.vie= player.vie_max
+            
+        if player.vie < 0 :
+             running= False
+  
     screen.blit(player.image,player.rect) # applique le curseur
     
     for k in Ennemis:
@@ -76,6 +94,6 @@ while running :
         
     
     pygame.display.flip()
-    vitesse.tick(169)
+    vitesse.tick(60) #fps
 
 pygame.quit() 
--- Projet chasse_taupe/Classe.py
+++ Projet chasse_taupe/Classe.py
@@ -3,10 +3,7 @@ import random
 import time
 
 
-class Game:
-    
-    def __init__(self):
-        self.player= Player()
+
 
 
 
@@ -20,47 +17,49 @@ class Player(pygame.sprite.Sprite):
         self.vie_max= 500
         self.degats= 50
         self.bdv=500
-        self.image= pygame.image.load('images/crosshair_1_50.png')
+        self.image= pygame.image.load('images/viseur.png')
         self.rect= self.image.get_rect()
    
-    def Barrede_vie(self,screen):
+    def Barrede_vie(self,screen):#On dessine 2 barres qui permettent de creer la barre de vie
        
-       pygame.draw.rect(screen,(244,203,164),[10,20,self.vie_max,10])
+       pygame.draw.rect(screen,(244,203,164),[10,20,self.vie_max,10])#C est cette barre la qui change
+       
        pygame.draw.rect(screen,(244,121,131),[10,20,self.vie,10])
    
-    def Gainde_vie(self):
+    def Gainde_vie(self): # quand on clique sur la taupe on gagne de la vie
         self.vie += 30
 
-    def perte_devie(self):
+    def perte_devie(self): # si on clqiue sur l ecran on perd la vie
         self.vie -=10
 
 class Taupe():
     
     def __init__(self):
         
-        taupeimage=['images/alien.png','images/mcdo.png','images/basique.png']
-        self.image=pygame.image.load(random.choice(taupeimage))
-    
         self.vie= 50
-        self.vie_max= 50
+        self.vie_max= 150
+        self.image= pygame.image.load("images/basique.png")
         self.degats= 1
         self.rect= self.image.get_rect()
         self.rect.x= 480
         self.rect.y= 345
     
-    nombreEnnemis= 2
-    
+    nombreEnnemis= 4 #On definit a 3 le nombre d ennemi
+   
+
     def Spawn(self):
         
-        positions = [  (480,345), (140,450),(20,362),(280,370),(710,410) ]
-        
+        positions = [  (480,345), (140,450),(20,362),(280,370),(710,410) ] #on definit une liste de positions
+        self.image= pygame.image.load("images/basique.png") #taupe 1 hit qui apparait a un lieu de base
         nid= random.choice(positions)
         self.rect.x= nid[0]  
         self.rect.y= nid[1]
-        self.vie+=1
+        self.vie=50
         
     
-    def Sustain(self):
-        self.vie+=1
+    def Sustain(self):#Dans le cas d un missclick notre taupe gagne des hps 
+        
+        self.vie+=50
         if self.vie > self.vie_max:
             self.vie= self.vie_max
+        
